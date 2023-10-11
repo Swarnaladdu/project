3a59615f8d2fddb201b2029f8cde2a31846c4f19
swarna <akepatiswarna05@gmail.com>
2023-10-11 20:56:30 +0530

    servicefile

---
+++ myapp/src/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Services extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is my service page</h1>
+            </div>
+        )
+    }
+}
+export default Services;
